b44005c95d360eafaaad706cc02540502e4b77fa
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-03-13 18:26:05 +0100

    esercizio finito

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,31 +2,37 @@
 import axios from "axios";
 export default {
   data() {
-    return {};
+    return {
+        projects:[],
+    };
   },
   methods: {},
   created() {
     axios.get("http://127.0.0.1:8000/api/projects").then((res) => {
       this.projects = res.data.results.data;
+      console.log(this.projects)
     });
   },
 };
 </script>
 
 <template>
-  <router-view></router-view>
+
   <div class="container">
     <h1>my project</h1>
     <div>
       <div class="row">
-        <div class="col-sm-6 mb-3 mb-sm-0">
+        <div class="col-12 mb-3 mb-sm-0 d-flex px-1 ">
           <div class="card" v-for="project in projects" :key="project.id">
             <div class="card-body">
-              <h5 class="card-title">{{ project.title }}</h5>
-              <p class="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
+              <h3 class="card-title">{{ project.title }}</h3>
+              <p class="card-text"> Created At: {{ project.date }}
               </p>
+              <p  class="card-text"> Type: {{ project.type.title }}
+              </p>
+              <p v-for="technology in project.technologies" :key="i" class="card-text"> Technologies: {{technology.title}}
+              </p>
+              
               
             </div>
           </div>
